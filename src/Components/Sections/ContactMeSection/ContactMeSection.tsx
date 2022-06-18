56d4c0c719eb117291269ad663294a68c6b86ae5
Adriel Oliveira <adriel.oliveira@dellead.com>
2022-06-18 02:09:47 -0300

    info gathering done, request missing

--- src/Components/Sections/ContactMeSection/ContactMeSection.tsx
+++ src/Components/Sections/ContactMeSection/ContactMeSection.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeaderTitleComponent } from '../../Shared/HeaderTitleComponent/HeaderTitleComponent';
 import { TitleComponent } from '../../Shared/TitleComponent';
 import './contact-me-section.scss';
 import Input from 'react-phone-number-input/input';
+import { Alert } from 'react-bootstrap';
 
 interface ContactMeSectionProps {}
 
 export default function ContactMeSection({}: ContactMeSectionProps) {
+	const [nome, setNome] = useState('');
+	const [email, setEmail] = useState('');
+	const [phone, setPhone] = useState('');
+
+	const onSubmit = (e: any) => {
+		e.preventDefault();
+		if (nome.length == 0 || email.length == 0 || phone.length == 0) {
+			alert('Campos Inválidos!');
+			return;
+		}
+
+		//request
+
+		alert('Obrigado por entrar em contato! ;)');
+		resetFields();
+	};
+
+	const resetFields = () => {
+		setNome('');
+		setEmail('');
+		setPhone('');
+	};
+
 	return (
 		<section className='contact-me-section'>
 			<HeaderTitleComponent style={{ color: 'black' }}>
@@ -25,22 +49,31 @@ export default function ContactMeSection({}: ContactMeSectionProps) {
 							placeholder='Nome'
 							className='rounded-button black-outline'
 							type='text'
+							onChange={(e) => {
+								setNome(e.target.value);
+							}}
 						/>
 						<input
 							placeholder='Email'
 							className='rounded-button black-outline'
 							type='email'
-							name=''
-							id=''
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
 						/>
 						<Input
 							placeholder='Telefone'
 							className='rounded-button black-outline'
-							onChange={() => {}}
+							onChange={(e) => {
+								if (e) {
+									setPhone(e);
+								}
+							}}
 							useNationalFormatForDefaultCountryValue
 							defaultCountry='BR'
 						/>
 						<input
+							onClick={onSubmit}
 							className='rounded-button black-outline black-button'
 							type='button'
 							value='Quero ser revendedor'
