3b5694c838e9f13ac8631ea207f5f9f7d9a16de8
Adriel Oliveira <adriel.oliveira@dellead.com>
2022-06-19 10:18:01 -0300

    fixed the vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import {defineConfig} from'vite'
 import mkcert from'vite-plugin-mkcert'
+import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     https: true
   },
-  plugins: [mkcert()]
+  plugins: [mkcert(), react()]
 })
