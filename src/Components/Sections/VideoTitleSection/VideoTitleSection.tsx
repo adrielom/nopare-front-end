1cae5d2ae685f6f549342f2db32503280afc7001
Adriel Oliveira <adriel.oliveira@dellead.com>
2022-06-19 10:21:59 -0300

    a few minor fixes

--- src/Components/Sections/VideoTitleSection/VideoTitleSection.tsx
+++ src/Components/Sections/VideoTitleSection/VideoTitleSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import TitleComponent from '../../Shared/TitleComponent';
+import { TitleComponent } from '../../Shared/TitleComponent';
 import VideoBackgroundComponent from '../../Shared/VideoBackgroundComponent';
 import './video-title-section.scss';
 
--- src/Components/Shared/HeaderTitleComponent/index.ts
+++ src/Components/Shared/HeaderTitleComponent/index.ts
@@ -1,1 +1,1 @@
-export { default } from './HeaderTitleComponent'
+export { HeaderTitleComponent } from './HeaderTitleComponent'
