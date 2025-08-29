d6ec901f733513d33a0d34f4bb4238a764342d5f
adrielom <adrielom.dev@gmail.com>
2025-08-29 20:40:16 -0300

    Added deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
+import {defineConfig} from'vite'
+import mkcert from'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [mkcert(), react()],
   assetsInclude: ['**/*.mov'],
-  base: './',
+  base: '/nopare-front-end/',
   server: {
     https: true
-  }
+  },
+  plugins: [mkcert(), react()]
 })
