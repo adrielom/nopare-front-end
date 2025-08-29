21d21795f80746583dce87039fab36555746830f
adrielom <adrielom.dev@gmail.com>
2025-08-29 19:37:03 -0300

    Added deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.mov'],
+  base: '/nopare-front-end/',
   server: {
     https: true
   },
